077d7920f0a5d09576f376c76c846a4c5c55a2af
Jake Soenneker <jake@soenneker.com>
2025-09-12 20:41:55 -0500

    Initial commit

---
+++ test/Soenneker.Quark.Switches.Tests/SwitchTests.cs
@@ -0,0 +1,18 @@
+﻿using Soenneker.Tests.FixturedUnit;
+using Xunit;
+
+namespace Soenneker.Quark.Switches.Tests;
+
+[Collection("Collection")]
+public sealed class SwitchTests : FixturedUnitTest
+{
+    public SwitchTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    {
+    }
+
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
